f4ae7959e3c6b50c2f0554df745fbd8669224284
MRzsztk <rzeszutek.malgorzata@gmail.com>
2020-09-06 20:31:06 +0200

    fix links

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -19,14 +19,14 @@ export default function Contact() {
                 <div className="contact-container">
                     <div className="contact-slot">
                         <img src={LinkedIn} className="linkedin" alt='linkedin' />
-                        <Link to={'www.linkedin.com/in/malgorzata-rzeszutek'}>linkedin.com/in/<b>malgorzata-rzeszutek</b></Link>
+                        <a href={'https://www.linkedin.com/in/malgorzata-rzeszutek'}>linkedin.com/in/<b>malgorzata-rzeszutek</b></a>
                     </div>
                     <div className="contact-slot">
                     <img src={Github} className="github" alt='github' />
-                    <Link to={'https://github.com/MRzsztk'}>github.com/<b>MRzsztk</b></Link></div>
+                    <a href={'https://github.com/MRzsztk'}>github.com/<b>MRzsztk</b></a></div>
                     <div className="contact-slot">
                     <img src={Fb} className="fb" alt='facebook' />
-                    <Link to={'https://www.facebook.com/rzeszutek.malgorzata'}>facebook.com/<b>rzeszutek.malgorzata</b></Link>
+                    <a href={'https://www.facebook.com/rzeszutek.malgorzata'}>facebook.com/<b>rzeszutek.malgorzata</b></a>
                     </div>
                     <h2 className="VT323">*** OR ***</h2>
                 <SendMeAMessage />
